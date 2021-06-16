138f4d6312806b1e01c9d2a79ec6f4154acba090
Ming Scott <ming.scott@gmail.com>
2021-06-16 15:43:18 -0300

    added build step to populate templates with images folder

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ for folder in *
 	do echo "$folder"
 	rm -rf "$folder"/patches
 	mkdir "$folder"/patches
+	mkdir "$folder"/images
 	cp ../{calico,ink}.js "$folder"/
 	cp -r ../patches/* "$folder"/patches
 	mkdir ../release
