e3abc68e1ce3ba77fc9d42b70caf6da49e2dba58
greencloversguy <35625742+greencloversguy@users.noreply.github.com>
2021-08-31 19:39:08 +0100

    Create audiohandler.js
    Created a music/sfx patch that allows music and sfx to be played using tags. It's basically a direct copy of how the image tags are handled.

---
+++ engine/patches/audiohandler.js
@@ -0,0 +1,83 @@
+// -----------------------------------
+// patch template
+// -----------------------------------
+
+var credits = {
+	emoji: "",
+	name: "name",
+	author: "author",
+	version: "1.0",
+	description: "description",
+	licences: {
+		self: "2021",
+	}
+}
+
+var options = {
+	patchname_variable: true,
+};
+
+let music_player = document.createElement("audio")
+let sfx_player = document.createElement("audio")
+
+
+
+Tags.add("music", (story, property)=>{
+		// make sure a file name was provided
+		console.log(property)
+		if (!typeof property === "string" || !property.trim()) 
+		{
+			warn.warn("(#music) no file was provided.");
+			return;
+		}
+		//If the player writes stop, stop the music. 
+		if (property.toLowerCase() === "stop"){
+			music_player.pause()
+		}
+		// if the image provided isn't a URL,
+		if (!property.startsWith("http"))
+		{
+			// ensure our file has a file extension
+			property = addFileType(property, story.options.defaultaudioformat, story.options.defaultaudiolocation);
+		}
+		music_player.src = property;
+		music_player.loop = true;
+		
+		music_player.play()
+	});
+
+Tags.add("sfx", (story, property)=>{
+	// make sure a file name was provided
+		console.log(property)
+		if (!typeof property === "string" || !property.trim()) 
+		{
+			warn.warn("(#sfx) no file was provided.");
+			return;
+		}
+		
+		// if the image provided isn't a URL,
+		if (!property.startsWith("http"))
+		{
+			// ensure our file has a file extension
+			property = addFileType(property, story.options.defaultaudioformat, story.options.defaultaudiolocation);
+		}
+
+		sfx_player.src = property;
+		sfx_player.loop = false;
+
+		sfx_player.play()
+});
+
+
+
+Patches.add(function()
+{
+	this.outerdiv.addEventListener("story ready", (event) =>
+	{
+		console.log(story)
+		this.outerdiv.appendChild(music_player);
+		this.outerdiv.appendChild(sfx_player);
+	});
+});
+
+export default {options: options, credits: credits};
